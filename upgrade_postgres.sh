af03dedc3cb85c3939e69c0c2cbf88edad1bcfa4
Jeff Wong <awole20@gmail.com>
2025-06-30 08:24:27 -0700

    common early exit if pg cluster exists

--- upgrade_postgres.sh
+++ upgrade_postgres.sh
@@ -14,6 +14,11 @@ docker_create_db_directories
 PG_MAJOR_OLD=$(find /var/lib/postgresql -name PG_VERSION -maxdepth 3 -type f -exec cat {} \; | sort -n | tail -n 1)
 PG_MAJOR_NEW=$(postgres --version | sed -rn 's/^[^0-9]*+([0-9]++).*/\1/p')
 
+# exit if pg major cluster exists
+if [ "${PG_MAJOR_NEW}" = "$PG_MAJOR_OLD" ]; then
+    exit 0
+fi
+
 # data dir/cluster name is /docker in newer versions, but data elsewhere. figure out /var/lib/postgres/{version}/{cluster}/*
 PG_MAJOR_OLD_CLUSTER=$(ls -1 /var/lib/postgresql/${PG_MAJOR_OLD} | tail -n 1)
 export PGDATAOLD=/var/lib/postgresql/${PG_MAJOR_OLD}/${PG_MAJOR_OLD_CLUSTER}
@@ -23,43 +28,39 @@ export PGBINNEW=/usr/lib/postgresql/${PG_MAJOR_NEW}/bin
 
 if [ "$(id -u)" = '0' ]; then
 	  # install old postgres then restart script as postgres user
-    if [ ! "${PG_MAJOR_NEW}" = "$PG_MAJOR_OLD" ]; then
-        echo "Installing PostgreSQL version ${PG_MAJOR_OLD} for upgrade.."
-        apt-get update
-        apt-get install -y postgresql-${PG_MAJOR_OLD} postgresql-${PG_MAJOR_OLD}-pgvector
-    fi
+    echo "Installing PostgreSQL version ${PG_MAJOR_OLD} for upgrade.."
+    apt-get update
+    apt-get install -y postgresql-${PG_MAJOR_OLD} postgresql-${PG_MAJOR_OLD}-pgvector
 	  exec gosu postgres "$BASH_SOURCE" "$@"
 fi
 
-if [ ! "${PG_MAJOR_NEW}" = "$PG_MAJOR_OLD" ]; then
-  echo Upgrading PostgreSQL from version ${PG_MAJOR_OLD} to ${PG_MAJOR_NEW}
-  free_disk=$(df -P -B1 /var/lib/postgresql | tail -n 1 | awk '{print $4}')
-  required=$(($(du -sb /var/lib/postgresql/${PG_MAJOR_OLD}/${PG_MAJOR_OLD_CLUSTER} | awk '{print $1}') * 2))
+echo Upgrading PostgreSQL from version ${PG_MAJOR_OLD} to ${PG_MAJOR_NEW}
+free_disk=$(df -P -B1 /var/lib/postgresql | tail -n 1 | awk '{print $4}')
+required=$(($(du -sb /var/lib/postgresql/${PG_MAJOR_OLD}/${PG_MAJOR_OLD_CLUSTER} | awk '{print $1}') * 2))
 
-  if [ "$free_disk" -lt "$required" ]; then
-    echo
-    echo -------------------------------------------------------------------------------------
-    echo "WARNING: Upgrading PostgreSQL would require an additional $(numfmt --to=si $(($required - $free_disk))) of disk space"
-    echo "Please free up some space, or expand your disk, before continuing."
-    echo
-    echo -------------------------------------------------------------------------------------
-    exit 1
-  fi
-
-	docker_init_database_dir
-	pg_setup_hba_conf "$@"
-  cd /var/run/postgresql
-  SUCCESS=true
-  ${PGBINNEW}/pg_upgrade --username="$POSTGRES_USER" || SUCCESS=false
-
-  if [[ "$SUCCESS" == 'false' ]]; then
-    echo -------------------------------------------------------------------------------------
-    echo UPGRADE OF POSTGRES FAILED
-    echo
-    echo -------------------------------------------------------------------------------------
-    exit 1
-  fi
-
-  echo "PostgreSQL version ${PG_MAJOR_OLD} upgrade complete"
-  exit 0
+if [ "$free_disk" -lt "$required" ]; then
+  echo
+  echo -------------------------------------------------------------------------------------
+  echo "WARNING: Upgrading PostgreSQL would require an additional $(numfmt --to=si $(($required - $free_disk))) of disk space"
+  echo "Please free up some space, or expand your disk, before continuing."
+  echo
+  echo -------------------------------------------------------------------------------------
+  exit 1
 fi
+
+docker_init_database_dir
+pg_setup_hba_conf "$@"
+cd /var/run/postgresql
+SUCCESS=true
+${PGBINNEW}/pg_upgrade --username="$POSTGRES_USER" || SUCCESS=false
+
+if [[ "$SUCCESS" == 'false' ]]; then
+  echo -------------------------------------------------------------------------------------
+  echo UPGRADE OF POSTGRES FAILED
+  echo
+  echo -------------------------------------------------------------------------------------
+  exit 1
+fi
+
+echo "PostgreSQL version ${PG_MAJOR_OLD} upgrade complete"
+exit 0
