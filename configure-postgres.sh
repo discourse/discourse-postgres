b0cfdbb94c99110acd89876630cfd19d5d06500e
Jeff Wong <awole20@gmail.com>
2025-07-06 10:31:58 -0700

    fix configure postgres missing trailing /

--- configure-postgres.sh
+++ configure-postgres.sh
@@ -5,9 +5,9 @@ if [ ! -f "${PGDATA}/postgresql.conf" ]; then
     exit 0
 fi
 
-sed -i 's/#?synchronous_commit *=.*/synchronous_commit = ${DB_SYNCHRONOUS_COMMIT}' ${CONFIG_FILE}
-sed -i 's/#?shared_buffers *=.*/shared_buffers = ${DB_SHARED_BUFFERS}' ${CONFIG_FILE}
-sed -i 's/#?work_mem *=.*/work_mem = ${DB_WORK_MEM}' ${CONFIG_FILE}
-sed -i 's/#?default_text_search_config *=.*/default_text_search_config = ${DB_DEFAULT_TEXT_SEARCH_CONFIG}' ${CONFIG_FILE}
-sed -i 's/#?logging_collector *=.*/logging_collector = ${DB_LOGGING_COLLECTOR}' ${CONFIG_FILE}
-sed -i 's/#?log_min_duration_statement *=.*/log_min_duration_statement = ${DB_LOG_MIN_DURATION_STATEMENT}' ${CONFIG_FILE}
+sed -i 's/#?synchronous_commit *=.*/synchronous_commit = ${DB_SYNCHRONOUS_COMMIT}/' ${CONFIG_FILE}
+sed -i 's/#?shared_buffers *=.*/shared_buffers = ${DB_SHARED_BUFFERS}/' ${CONFIG_FILE}
+sed -i 's/#?work_mem *=.*/work_mem = ${DB_WORK_MEM}/' ${CONFIG_FILE}
+sed -i 's/#?default_text_search_config *=.*/default_text_search_config = ${DB_DEFAULT_TEXT_SEARCH_CONFIG}/' ${CONFIG_FILE}
+sed -i 's/#?logging_collector *=.*/logging_collector = ${DB_LOGGING_COLLECTOR}/' ${CONFIG_FILE}
+sed -i 's/#?log_min_duration_statement *=.*/log_min_duration_statement = ${DB_LOG_MIN_DURATION_STATEMENT}/' ${CONFIG_FILE}
