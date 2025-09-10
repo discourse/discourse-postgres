c914fc87055f6a56676a70ad2a4610383485dbd9
Jeff Wong <awole20@gmail.com>
2025-09-09 19:44:37 -0700

    bootstrap-db: Do not stop on error
    Allow subsequent statements to run, allows for password updates on user exists

--- bootstrap-db.sh
+++ bootstrap-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -z "$DB_USER" ] && [ ! "$DB_USER" = "$POSTGRES_USER" ]; then
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $DB_USER;
 GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $DB_USER;
 ALTER SCHEMA PUBLIC owner TO $DB_USER;
@@ -11,14 +11,14 @@ EOSQL
 fi
 
 if [ ! "$SKIP_EXTENSIONS" = "1" ]; then
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "template1" <<-EOSQL
+    psql --username "$POSTGRES_USER" --dbname "template1" <<-EOSQL
 CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS vector;
 ALTER EXTENSION vector UPDATE;
 EOSQL
 
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS vector;
